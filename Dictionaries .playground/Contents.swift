ff929b7b790a622398f8660db48ce256cb8b06aa
Trym Lintzen <Trymlintzen@Macintosh-van-Trym.local>
2017-10-03 15:23:51 +0200

    update

---
+++ Dictionaries .playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+// Dictionaries
+// something with a key (top gun) and a value (1)
+
+var movieRatings = ["Top Gun" : 1 , "Star Wars" : 10, "Indiana Jones" : 9]
+
+movieRatings ["Top Gun"] = 5   // veranderde key value naar 5
+
+movieRatings.updateValue(10, forKey: "Pulp fiction")
+
+for(key, value) in movieRatings {
+    print(key)
+    print(value)
+}
+
+// Exercise
+
+
+var EnglishDictonary: Dictionary<String, String> = ["hello": "greet"]
+var DutchDictionary: Dictionary<String, String> = ["hallo": "begroeting", "ney":"no"]
+var FrenchDictionary: Dictionary<String, String> = ["bonjour": "frans begroeting"]
+
+var arryDictionary = [EnglishDictonary, DutchDictionary, FrenchDictionary]
+
+
+for currentDictionary in arryDictionary {
+    print(currentDictionary)
+    if currentDictionary == EnglishDictonary {
+        print(currentDictionary["hello"])
+    } else if currentDictionary == DutchDictionary {
+        print(currentDictionary["ney"])
+    } else {
+        print("nothing")
+    }
+}
+
+
+var movieRating: Dictionary<String, Int> = ["Interstellar": 9 , "Pulp fiction" : 8]
+print(movieRating)
+movieRating.updateValue(7, forKey: "Interstellar")
+print(movieRating)
+
+// Exercise 2
+
+
+
+
+
---
+++ Praxis dictinoaries.playground/Contents.swift
@@ -0,0 +1,56 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Here is the escaped string...just imagine this is our data retrieved from a server! Like for real!!!
+var itemsJSONString = "{\n\t\"PraxisItemsForSale\": [{\n\t\t\"id\": 1,\n\t\t\"name\": \"A green door\",\n\t\t\"price\": 12.50,\n\t\t\"tags\": [\"home\", \"green\"]\n\t}, {\n\t\t\"id\": 2,\n\t\t\"name\": \"A saw\",\n\t\t\"price\": 10,\n\t\t\"tags\": [\"garden\", \"home\"]\n\t}]\n}"
+
+
+//The clever parsed, it deserialises the JSON, don't worry about it too much lot of complicated things here
+extension String {
+    
+    var parseJSONString: AnyObject? {
+        
+        let data = self.data(using: String.Encoding.utf8, allowLossyConversion: false)
+        
+        if let jsonData = data {
+            do {
+                let message = try JSONSerialization.jsonObject(with: jsonData, options:.mutableContainers)
+                return message as AnyObject?
+            }
+            catch let error as NSError
+            {
+                print("An error occurred: \(error)")
+                return nil
+            }
+            
+            // Will return an object or nil if JSON decoding fails
+        } else {
+            // Lossless conversion of the string was not possible
+            return nil
+        }
+    }
+}
+
+let itemsDictionary = itemsJSONString.parseJSONString as? NSDictionary
+if let praxisItemsForSale = itemsDictionary!["PraxisItemsForSale"] as? NSArray {
+    print(itemsDictionary)
+    for item in praxisItemsForSale {
+        if let itemDict = item as? Dictionary<String, AnyObject>,
+            let name = itemDict["name"] as? String,
+            let price = itemDict ["price"] as? Double,
+            let tags = itemDict["tags"] as? NSArray {
+                print(itemDict)
+                print(name)
+                print(price)
+                for tag in tags{
+                    print(tag)
+                }
+        }
+    }
+} else {
+    print("Wrong key")
+}
+
+// Unpack de dictionarries
+
